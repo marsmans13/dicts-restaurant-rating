250b373d6385f0a18503b1a20a6b16d95cbdde8b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-11 16:29:32 -0700

    added random restaurant feature

--- ratings.py
+++ ratings.py
@@ -1,5 +1,6 @@
 """Restaurant rating lister."""
 import sys
+from random import choice
 
 file_name = sys.argv[1]
 restaurant_info = {}
@@ -12,29 +13,43 @@ for line in open(file_name):
 def get_user_input():
     print("If you would like to see all restaurants, type 1.")
     print("If you would like to add a restaurant and its rating, type 2.")
-    print("If you want to quit, type 3.")
+    print("If you would like to update a restaurant, type 3.")
+    print("If you want to quit, type 4.")
     return input()
 
 
-user_input = get_user_input()
-
-while user_input != "3":
-
-    if user_input == "1":
-        for key in sorted(restaurant_info):
-            print(key, "is rated at", restaurant_info[key] + ".")
-
-    elif user_input == "2":
-
-        new_restaurant = input("Please give us a restaurant name: ")
+def test_user_rating(restaurant_name):
         new_rating = input("Please give us a rating from 1-5: ")
 
         while new_rating not in "12345":
             print("Please enter a number between 1 and 5.")
             new_rating = input()
 
-        print("\n")
+        restaurant_info[restaurant_name] = new_rating
 
-        restaurant_info[new_restaurant] = new_rating
+
+user_input = get_user_input()
+
+while user_input != "4":
+
+    if user_input == "1":
+        for key in sorted(restaurant_info):
+            print(key, "is rated at", restaurant_info[key] + ".")
+
+    elif user_input == "2":
+        new_restaurant = input("Please give us a restaurant name: ")
+        test_user_rating(new_restaurant)
+
+    elif user_input == "3":
+        user_random = input("Do you want to update a random restaurant? y/n: ")
+
+        if user_random.lower() == "y":
+            random_restaurant = choice(list(restaurant_info.keys()))
+            print(random_restaurant, "is rated at",
+                  restaurant_info[random_restaurant] + ".")
+            test_user_rating(random_restaurant)
+
+        elif user_random.lower() == "n":
+            pass
 
     user_input = get_user_input()
